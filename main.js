d60db71ae91bd721183f79b2d85310295ad4171c
andy.rothwell <rothwell.andy@gmail.com>
2017-12-15 10:55:02 -0500

    removes es6 from main.js, removes vestigial recordings-client.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log = console.info = console.debug = console.error = function () {};
 
-const GATEKEEPER_KEY = '35ae5b7bf8f0ff2613134935ce6b4c1e';
+var GATEKEEPER_KEY = '35ae5b7bf8f0ff2613134935ce6b4c1e';
 
 Mapboard.default({
   baseConfig: '//rawgit.com/rbrtmrtn/mapboard-base-config/9605e5dca32277b1b877e8965d2156631b0b7443/config.js',
