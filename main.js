bf341fbdcf8a2a63e516d3213750b4ef3b9b7999
andy.rothwell <rothwell.andy@gmail.com>
2017-12-15 12:19:35 -0500

    uses later version of baseconfig which has no es6, comments out unused scripts

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log = console.info = console.debug = console.error = function () {};
 var GATEKEEPER_KEY = '35ae5b7bf8f0ff2613134935ce6b4c1e';
 
 Mapboard.default({
-  baseConfig: '//rawgit.com/rbrtmrtn/mapboard-base-config/9605e5dca32277b1b877e8965d2156631b0b7443/config.js',
+  baseConfig: '//rawgit.com/rbrtmrtn/mapboard-base-config/d9892943dc4df94ba2b1e51dbe2aeb0b0bcd4aab/config.js',
   cyclomedia: {
     enabled: true
   },
