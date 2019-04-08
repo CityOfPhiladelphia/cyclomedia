b6517dc6c3379a64ea2055b348fc785e72723906
andy.rothwell <rothwell.andy@gmail.com>
2019-04-08 16:03:58 -0400

    switches base config, adds to gitignore

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 console.log = console.info = console.debug = console.error = function () {};
 
-var GATEKEEPER_KEY = '35ae5b7bf8f0ff2613134935ce6b4c1e';
+// var GATEKEEPER_KEY = '35ae5b7bf8f0ff2613134935ce6b4c1e';
+var BASE_CONFIG_URL = 'https://cdn.jsdelivr.net/gh/ajrothwell/mapboard-base-config@f1802a4bef28265e2add0e94ce823852a5341ad6/config.js';
 
 Mapboard.default({
-  baseConfig: 'https://rawgit.com/rbrtmrtn/mapboard-base-config/d9892943dc4df94ba2b1e51dbe2aeb0b0bcd4aab/config.js',
+  baseConfig: BASE_CONFIG_URL,
   cyclomedia: {
     enabled: true
   },
