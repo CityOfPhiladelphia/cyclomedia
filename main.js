71197bbf79a68d854d0c9731ede33a8ec8e0a105
andy.rothwell <rothwell.andy@gmail.com>
2019-04-11 14:11:20 -0400

    fix base config

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log = console.info = console.debug = console.error = function () {};
 
 // var GATEKEEPER_KEY = '35ae5b7bf8f0ff2613134935ce6b4c1e';
-var BASE_CONFIG_URL = 'https://cdn.jsdelivr.net/gh/ajrothwell/mapboard-base-config@f1802a4bef28265e2add0e94ce823852a5341ad6/config.js';
+var BASE_CONFIG_URL = 'https://cdn.jsdelivr.net/gh/rbrtmrtn/mapboard-base-config@d9892943dc4df94ba2b1e51dbe2aeb0b0bcd4aab/config.js';
 
 Mapboard.default({
   baseConfig: BASE_CONFIG_URL,
