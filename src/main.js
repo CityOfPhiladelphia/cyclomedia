a354cf52c0a43cebc161c789859ecc8b2e2c427c
Andy Rothwell <Andy.Rothwell@phila.gov>
2022-02-01 16:15:49 -0500

    testing cloudfront function switchover

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import viewerboard from '@phila/viewerboard/src/main.js';
 
 viewerboard({
   app: {
-    title: 'Cyclomedia Dev test 04',
+    title: 'Cyclomedia Dev test 05',
     tagLine: '',
   },
   cyclomedia: {
